9d90726bccedb33b6b113de493731f1d6f6b7677
Christoph <christoph_rohde@icloud.com>
2024-02-04 15:31:49 +0100

    Add RGBA

---
+++ Font/RGBA.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Christoph Rohde on 04.02.24.
+//
+
+#pragma once
+#include <iostream>
+#include "RGB.hpp"
+
+struct RGBA {
+    std::uint8_t red = 0;
+    std::uint8_t green = 0;
+    std::uint8_t blue = 0;
+    std::uint8_t alpha = 1;
+};
+
+
